de383e61d47d070b82e99639a80a61c7b903fe82
Ryo <ryonotjs9@gmail.com>
2026-01-01 16:53:31 +0800

    1

---
+++ scrip.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center min-h-screen bg-gray-100">
+      <a 
+        href="https://github.com/username/repo" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/gambar.png"
+          alt="Github Image"
+          width={300}
+          height={300}
+          className="rounded-xl hover:scale-105 transition cursor-pointer shadow-lg"
+        />
+      </a>
+    </main>
+  );
+}
